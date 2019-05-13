675f437e9634b0b871000f7499108bc7433d7fcb
xxlxep <xxlxep@gmail.com>
2019-05-13 13:29:57 +0300

    Image loader added

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,10 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+      }
     ],
   },
 };
